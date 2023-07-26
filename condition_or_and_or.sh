7e4d2d45cb8e1259b0be42ead84e687c18062a1b
Ryoichi <ryoichikakehi@gmail.com>
2023-07-26 19:16:26 +0900

    modified:   condition_or_and_or.sh 	new file:   readonly_local.sh

--- condition_or_and_or.sh
+++ condition_or_and_or.sh
@@ -5,7 +5,8 @@ readonly FILEA=${CWD}/A.dat
 readonly FILEB=${CWD}/B.dat
 
 [[ ! -f ${FILEA} || ! -f ${FILEB} ]] && echo "Either FILEA or FILEB or both do not exist."
-[[ ! -f ${FILEA} ]] && echo "No FILEA"
-[[ ! -f ${FILEB} ]] && echo "No FILEB"
-[[ -f ${FILEA}   ]] && echo "FILEA"
-[[ -f ${FILEB}   ]] && echo "FILEB"
+[[ ! -f ${FILEA} ]] && echo "No FILEA" || echo "FILEA"
+[[ ! -f ${FILEB} ]] && echo "No FILEB" || echo "FILEB"
+[[ -f ${FILEA}   ]] && echo "FILEA" || echo "No FILEA"
+[[ -f ${FILEB}   ]] && echo "FILEB" || echo "No FILEB"
+
